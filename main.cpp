31cefd40e158edd2bf51588afb91c8120d5facca
krzyjast1 <krzyjast1@gmail.com>
2016-06-15 23:04:44 +0200

    Create Project
    Made main file and only this

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    Application app;//INSTANCE OF APPLICATION :TODO: -> Check what must be setted by constructor
+
+    app.runApplication();//RUN APPLICATION
+
+    return 0;//END WITH NO PROBLEMS
+}
